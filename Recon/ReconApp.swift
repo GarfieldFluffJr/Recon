ea60adfb7942482d855a1746ef024df5b2f27e3f
Louie <louieyin6@gmail.com>
2026-02-10 18:20:28 -0500

    Initial Commit

---
+++ Recon/ReconApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReconApp.swift
+//  Recon
+//
+//  Created by Louie Yin on 2026-02-10.
+//
+
+import SwiftUI
+
+@main
+struct ReconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
