e22b78af6a79f576a964c753241bcea6c98f6e9f
Louie <louieyin6@gmail.com>
2026-02-24 18:43:38 -0500

    api config

--- Recon/Config/APIConfig.swift
+++ Recon/Config/APIConfig.swift
@@ -5,3 +5,12 @@
 //  Created by Louie Yin on 2026-02-24.
 //
 
+import Foundation
+
+enum APIConfig {
+    // AWS API Gateway
+    static let baseURL = "https://wfpzuuwq09.execute-api.us-east-1.amazonaws.com"
+    // http paths
+    static let uploadURL = "\(baseURL)/upload-url"
+    static let analyzeURL = "\(baseURL)/analyze"
+}
