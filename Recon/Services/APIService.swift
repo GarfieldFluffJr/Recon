a5493267dc8c0015ccfede2550804c6a9efddc4a
Louie <louieyin6@gmail.com>
2026-02-24 18:43:38 -0500

    api config

--- Recon/Services/APIService.swift
+++ Recon/Services/APIService.swift
@@ -5,3 +5,15 @@
 //  Created by Louie Yin on 2026-02-24.
 //
 
+// Handle HTTP requests to Lambda
+
+import Foundation
+
+class APIService {
+    
+    // Get presigned upload url from Lambda
+    func getUploadURL() async throws -> (uploadUrl: String, videoKey: String) {
+        let url = URL(string: APIConfig.uploadURL)!
+        
+    }
+}
